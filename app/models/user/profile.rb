4844966e0b0529577c0a2198857fe692857f7191
Aivils Štoss <aivils@latnet.lv>
2020-11-02 08:04:37 +0200

    Profile code validation

--- app/models/user/profile.rb
+++ app/models/user/profile.rb
@@ -2,5 +2,5 @@ class User::Profile < ApplicationRecord
   DISPLAY_TIMES = [30, 60, 120]
   belongs_to :user
 
-  validates :code, format: { with: /\A[-a-z0-9]+\Z/i, message: :code_is_invalid }
+  validates :code, format: { with: /\A[-a-z0-9]+\Z/i, message: :code_is_invalid }, allow_nil: true
 end
