ecb2f7cca4caed6d34c68c545c5cb629f45626dc
Aivils Štoss <aivils@latnet.lv>
2023-12-03 11:43:46 +0200

    new production host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,11 +4,11 @@ set :branch, :master
 set :deploy_to, "/home/rails/#{fetch(:stage)}"
 set :user, 'rails'
 
-role :app, %w{139.59.43.193}
-role :web, %w{139.59.43.193}
-role :db,  %w{139.59.43.193}
+role :app, %w{139.59.58.44}
+role :web, %w{139.59.58.44}
+role :db,  %w{139.59.58.44}
 
-server '139.59.43.193',
+server '139.59.58.44',
   user: 'rails',
   roles: %w{web app}
 
