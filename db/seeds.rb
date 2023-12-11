08e1ccd16eec488296d65a8806d0bd334f9bdbef
Aivils Štoss <aivils@latnet.lv>
2023-12-11 14:13:42 +0200

    docker env, db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@
 Role::ROLES.each do |name|
   Role.where(name: name).first_or_create
 end
+
+role = Role.find_by(name: "Admin")
+user = User.where(email: 'admin@admin.com').first_or_initialize
+if user.new_record?
+  user.role_ids = [role.id]
+  user.password = '12345678'
+  user.password_confirmation = '12345678'
+  user.save
+end
+
