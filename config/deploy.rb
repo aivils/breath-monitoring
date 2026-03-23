c46e935eb5a6f87ce73ed19614820f66a6b3d657
Aivils Štoss <aivils@latnet.lv>
2026-03-23 08:39:04 +0200

    production storage

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :repo_url, "git@github.com:aivils/breath-monitoring.git"
 append :linked_files, "config/database.yml", "config/master.key", "config/credentials/production.key", ".env.production"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache" #, "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "storage" #, "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
